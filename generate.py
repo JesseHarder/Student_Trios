c7382ee024cb952971bc21d225aff2dfc3dd0e67
JesseHarder <jhharder@scu.edu>
2017-11-15 14:28:26 -0800

    Read input file name from command line.

--- generate.py
+++ generate.py
@@ -1,6 +1,12 @@
+import sys
+
 if __name__ == "__main__":
-    print("Hi.")
-    # Get input file name from commandline.
+    # Get input file name.
+    if len(sys.argv) == 1:
+        print("ERROR: Please give input file name as commandline argument.")
+
+    filename = sys.argv[1]
+
     # Open file.
     # Read input.
     # Write result to lp file.
