c1f9e355403a520e684655d1473231388f652873
JesseHarder <jhharder@scu.edu>
2017-11-16 17:08:21 -0800

    Merge was premature. XD Generator bugs fixed now. (Hopefully.)

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 import sys
 import csv
 
-output_filename = "output/results.pl"
+output_filename = "preferences/results.lp"
 
 
 # Function for generating the output file.
@@ -13,14 +13,27 @@ def generate_preference_file(out_filename, students, student_preferences):
         out_file.write('% Read "Student1 ranks Student2 with preference Preference,\n')
         out_file.write('%       where higher Preference is better."\n\n')
 
-        # Print Student Preference Rules
+        # Print Student Preference Facts
+        vetos = []
         for ranking_student in student_preferences:
             prefs = student_preferences[ranking_student]
-            for i in range(0,len(prefs)):
+            for i in range(0, len(prefs)):
                 ranked_student = students[i]
-                pref = prefs[i]
-                out_file.write('pref("%s", "%s", %s).\n' % (ranking_student, ranked_student, pref))
+                # Students don't have preferences for working with themselves.
+                if ranking_student == ranked_student:
+                    continue
 
+                pref = prefs[i]
+                # Vetos recorded for later printing.
+                if str.upper(pref) == "X":
+                    vetos.append({"vetoer": ranking_student, "vetoee": ranked_student})
+                    continue
+
+                out_file.write('pref("%s", "%s", %s).\n' % (ranking_student, ranked_student, pref))
+            out_file.write("\n")
+        # Print Veto Facts
+        for veto in vetos:
+            out_file.write('vetos("%s", "%s").\n' % (veto['vetoer'], veto['vetoee']))
 
 
 
@@ -41,11 +54,11 @@ if __name__ == "__main__":
             rows.append(row)
 
     # Reformat input for easy use.
-    people = rows[0][1:len(rows[0]) - 1]
+    people = rows[0][1:len(rows[0])]
     preferences = {}
-    for row in rows[1:len(rows[0]) - 1]:
+    for row in rows[1:len(rows[0])]:
         name = row[0]
-        prefs = row[1:len(row) - 1]
+        prefs = row[1:len(row)]
         preferences[name] = prefs
 
     # Accept output filename as command line argument.
